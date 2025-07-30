6aa591db1c653855350bbe499e811e785d550d4f
EC2 Default User <ec2-user@ip-172-31-4-79.ap-southeast-2.compute.internal>
2025-07-30 11:37:25 +0000

    [Update] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,21 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+ 
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,6 +23,27 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+
+
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト 
   end
 
   private
